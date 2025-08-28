42d9d591265449ae9682e6a6acc914d2d84ab260
olivioCk <oliviosevero4@gmail.com>
2025-08-28 23:48:41 +0000

    primeira

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
 
+int main(){
+   char Estado[50],Codigo,Cidade;
+   int população, pontosturisticos;
+   float Área,PIB;
+
+   printf ("Digite o estado: \n");
+   scanf ("%s", &Estado);
+
+   Printf("Digite o codigo: \n");
+   scanf("%s", &Codigo);
+
+   printf ("Digite sua Cidade: \n");
+   scanf ("%s", &Cidade);
+
+   printf("Digite a população: \n");
+   scanf("%d", &população);
+
+   Printf("Digite pontosturisticos: \n");
+   scanf("%d", &pontosturisticos);
+
+   printf("Digite sua Área: \n");
+   scanf("%f", &Área);
+   
+   printf("Digite seu PIB: \n");
+   scanf("%f", &PIB);
+
+   
+
+
+
+}
