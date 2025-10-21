435ccc3150f4370a1f0a9ccb65ae2bbfdf062b2b
olivioCk <oliviosevero4@gmail.com>
2025-10-21 20:36:55 +0000

    Xadrez nivel xadrez_mestre

--- super_trunfo.c
+++ super_trunfo.c
@@ -37,15 +37,7 @@ int main() {
     scanf("%f", &PIB01);
 
    
-    printf("\nDigite o nome da carta 2: ");
-    scanf("%s", carta02);
-    printf("Digite o codigo: ");
-    scanf("%s", codigo02);
-    printf("Digite o estado: ");
-    scanf("%s", estado02);
-    printf("Digite a cidade: ");
-    scanf("%s", cidade02);
-    printf("Digite a populacao: ");
+    printf("\nDigite o nome da care a populacao: ");
     scanf("%lu", &populacao02);
     printf("Digite os pontos turisticos: ");
     scanf("%d", &pontosTuristicos02);
@@ -57,11 +49,11 @@ int main() {
     
     densidade01 = populacao01 / area01;
     PIBPerCapita01 = (PIB01 * 1000000000.0) / populacao01;
-    super_poder01 = (float)populacao01 + area01 + PIB01 + pontosTuristicos01 + PIBPerCapita01 + (1.0f / densidade01);
+    super_poder01 = (float)populacao01 + area01 + PIB01 + (float) pontosTuristicos01 + PIBPerCapita01 + (1.0f / densidade01);
 
     densidade02 = populacao02 / area02;
     PIBPerCapita02 = (PIB02 * 1000000000.0) / populacao02;
-    super_poder02 = (float)populacao02 + area02 + PIB02 + pontosTuristicos02 + PIBPerCapita02 + (1.0f / densidade02);
+    super_poder02 = (float)populacao02 + area02 + PIB02 +(float) pontosTuristicos02 + PIBPerCapita02 + (1.0f / densidade02);
 
    
     printf("CARTA 1\n");
@@ -90,10 +82,7 @@ int main() {
     printf("PIB: Carta 1 venceu? %d\n", PIB01 > PIB02);
     printf("Pontos Turisticos: Carta 1 venceu? %d\n", pontosTuristicos01 > pontosTuristicos02);
     printf("Densidade: Carta 1 venceu? %d\n", densidade01 < densidade02); // menor vence
-    printf("PIB per capita: Carta 1 venceu? %d\n", PIBPerCapita01 > PIBPerCapita02);
-    printf("Super Poder: Carta 1 venceu? %d\n", super_poder01 > super_poder02);
-
-    return 0;
+    
 }
 
 
